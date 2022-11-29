71d583ae5feb3c0bfcb926fe1031bc3333a56b4c
Rohith Sanjay <rohiths2@illinois.edu>
2022-11-29 19:36:25 +0000

    prepared demo in main function

--- entry/main.cpp
+++ entry/main.cpp
@@ -2,15 +2,17 @@
 #include "../src/data_parser.cpp"
 #include "../src/graph.cpp"
 
-int main() {
-    std::cout << "Parsing data..." << std::endl;
-    DataParser d;
-    d.populateAirportRows("../lib/airports.dat");
-    d.populateRoutesRows("../lib/routes.dat");
-    d.populateAirportsDetails();
-    d.populateRoutesDetails();
-    d.checkMissingInfo();
-    Graph g = Graph(d);
+
+
+void user_control() {
+  std::cout << "Parsing data..." << std::endl;
+  DataParser d;
+  d.populateAirportRows("../lib/airports.dat");
+  d.populateRoutesRows("../lib/routes.dat");
+  d.populateAirportsDetails();
+  d.populateRoutesDetails();
+  d.checkMissingInfo();
+  Graph g = Graph(d);
 
   char x;
   std::cout << "Please type the number corresponding to what you would like to do, then press enter:" << std::endl;
@@ -50,12 +52,12 @@ int main() {
     std::cout << std::endl;
 
   } else if (x == '2') {
-    std::string iata;
-    std::cout << "Type the 3-letter airport code (IATA), then press enter" << std::endl;
-    std::cin >> iata;
-    for (size_t i = 0; i < iata.size(); ++i) {
-      iata[i] = toupper(iata[i]);
-    }
+  std::string iata;
+  std::cout << "Type the 3-letter airport code (IATA), then press enter" << std::endl;
+  std::cin >> iata;
+  for (size_t i = 0; i < iata.size(); ++i) {
+    iata[i] = toupper(iata[i]);
+  }
 
     Graph::Airport a = g.getAirportFromIATA_(iata);
     std::string country = a.country_;
@@ -140,55 +142,70 @@ int main() {
   } else {
     std::cout << "Invalid input option" << std::endl;
   }
-//  DataParser d;
-// d.populateAirportRows("../lib/airports.dat");
-// d.populateRoutesRows("../lib/routes.dat");
-// d.populateAirportsDetails();
-// d.populateRoutesDetails();
-// d.checkMissingInfo();
-// std::cout << "Airports Details for First 20 Airports" << std::endl;
-// for (int i = 0; i < 20; ++i) {
-//   std::cout << "Name: " << d.AirportsDetails[i][1] << "       --- City: " << d.AirportsDetails[i][2] << "        --- Code: " << d.AirportsDetails[i][4] << std::endl;
-// }
-// std::cout << std::endl;
-// std::cout << "Routes Details for First 20 Routes" << std::endl;
-// for (int i = 0; i < 20; ++i) {
-//   std::cout << "Source: " << d.RoutesDetails[i][2] << "       --- Dest: " << d.RoutesDetails[i][4] << std::endl;
-// }
-// std::cout << std::endl;
-// Graph g = Graph(d);
-// std::cout << "Details for ORD" << std::endl;
-// Graph::Airport ord = g.getAirportFromIATA_("ORD");
-// std::cout << "Name: " << ord.name_ << " --- City: " << ord.city_ << " --- Country " << ord.country_ << " --- Latitude " << ord.latitude_ << " --- Longitude " << ord.longitude_ << std::endl;
-// std::cout << std::endl;
-// std::cout << "Details for LAX" << std::endl;
-// Graph::Airport lax = g.getAirportFromIATA_("LAX");
-// std::cout << "Name: " << lax.name_ << " --- City: " << lax.city_ << " --- Country " << lax.country_ << " --- Latitude " << lax.latitude_ << " --- Longitude " << lax.longitude_ << std::endl;
 
-// std::cout << std::endl;
-// std::cout << "Adjacency List" << std::endl;
-// g.populateConnectionsIATA_country(d, "United States");
-// std::cout << "Connections From MDW" << std::endl;
-// std::vector<std::string> v = g.getConnectionsIATA().find("MDW")->second;
-// for (auto a : v) {
-//   std::cout << a << ", ";
+  char z;
+  std::cout << std::endl;
+  std::cout << "Type in anything and press enter to run the program again. Press CTRL+C to stop." << std::endl;
+  std::cin >> z;
+}
+
+
+
+
+
+int main() {
+// while(true) {
+//   user_control();
 // }
-// std::cout << std::endl;
-// std::cout << std::endl;
-// std::cout << "Connections From AUS" << std::endl;
-// std::vector<std::string> w = g.getConnectionsIATA().find("AUS")->second;
-// for (auto a : w) {
-//   std::cout << a << ", ";
-// }
-// std::cout << std::endl;
-// std::cout << std::endl;
-// std::cout << "BFS Traversal from SAB to SBH" << std::endl;
-// g.populateConnectionsIATA(d);
-// g.BFS("SAB", "SBH", false);
-// std::cout << std::endl;
-// std::cout << std::endl;
-// std::cout << "BFS Traversal from ORD to LAX" << std::endl;
-// g.populateConnectionsIATA(d);
-// g.BFS("ORD", "LAX", false);
+ DataParser d;
+d.populateAirportRows("../lib/airports.dat");
+d.populateRoutesRows("../lib/routes.dat");
+d.populateAirportsDetails();
+d.populateRoutesDetails();
+d.checkMissingInfo();
+std::cout << "Airports Details for First 20 Airports" << std::endl;
+for (int i = 0; i < 20; ++i) {
+  std::cout << "Name: " << d.AirportsDetails[i][1] << "       --- City: " << d.AirportsDetails[i][2] << "        --- Code: " << d.AirportsDetails[i][4] << std::endl;
+}
+std::cout << std::endl;
+std::cout << "Routes Details for First 20 Routes" << std::endl;
+for (int i = 0; i < 20; ++i) {
+  std::cout << "Source: " << d.RoutesDetails[i][2] << "       --- Dest: " << d.RoutesDetails[i][4] << std::endl;
+}
+std::cout << std::endl;
+Graph g = Graph(d);
+std::cout << "Details for ORD" << std::endl;
+Graph::Airport ord = g.getAirportFromIATA_("ORD");
+std::cout << "Name: " << ord.name_ << " --- City: " << ord.city_ << " --- Country " << ord.country_ << " --- Latitude " << ord.latitude_ << " --- Longitude " << ord.longitude_ << std::endl;
+std::cout << std::endl;
+std::cout << "Details for LAX" << std::endl;
+Graph::Airport lax = g.getAirportFromIATA_("LAX");
+std::cout << "Name: " << lax.name_ << " --- City: " << lax.city_ << " --- Country " << lax.country_ << " --- Latitude " << lax.latitude_ << " --- Longitude " << lax.longitude_ << std::endl;
+
+std::cout << std::endl;
+std::cout << "Adjacency List" << std::endl;
+g.populateConnectionsIATA_country(d, "United States");
+std::cout << "Connections From MDW" << std::endl;
+std::vector<std::string> v = g.getConnectionsIATA().find("MDW")->second;
+for (auto a : v) {
+  std::cout << a << ", ";
+}
+std::cout << std::endl;
+std::cout << std::endl;
+std::cout << "Connections From AUS" << std::endl;
+std::vector<std::string> w = g.getConnectionsIATA().find("AUS")->second;
+for (auto a : w) {
+  std::cout << a << ", ";
+}
+std::cout << std::endl;
+std::cout << std::endl;
+std::cout << "BFS Traversal from SAB to SBH" << std::endl;
+g.populateConnectionsIATA(d);
+g.BFS("SAB", "SBH", false);
+std::cout << std::endl;
+std::cout << std::endl;
+std::cout << "BFS Traversal from ORD to LAX" << std::endl;
+g.populateConnectionsIATA(d);
+g.BFS("ORD", "LAX", false);
  return 1;
 }
