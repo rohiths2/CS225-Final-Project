94c31de6f223259707b8777f504e5df284babbe9
Rohith Sanjay <rohiths2@illinois.edu>
2022-11-29 20:06:46 +0000

    changed main function

--- entry/main.cpp
+++ entry/main.cpp
@@ -154,9 +154,7 @@ void user_control() {
 
 
 int main() {
-// while(true) {
-//   user_control();
-// }
+
  DataParser d;
 d.populateAirportRows("../lib/airports.dat");
 d.populateRoutesRows("../lib/routes.dat");
@@ -172,40 +170,8 @@ std::cout << "Routes Details for First 20 Routes" << std::endl;
 for (int i = 0; i < 20; ++i) {
   std::cout << "Source: " << d.RoutesDetails[i][2] << "       --- Dest: " << d.RoutesDetails[i][4] << std::endl;
 }
-std::cout << std::endl;
-Graph g = Graph(d);
-std::cout << "Details for ORD" << std::endl;
-Graph::Airport ord = g.getAirportFromIATA_("ORD");
-std::cout << "Name: " << ord.name_ << " --- City: " << ord.city_ << " --- Country " << ord.country_ << " --- Latitude " << ord.latitude_ << " --- Longitude " << ord.longitude_ << std::endl;
-std::cout << std::endl;
-std::cout << "Details for LAX" << std::endl;
-Graph::Airport lax = g.getAirportFromIATA_("LAX");
-std::cout << "Name: " << lax.name_ << " --- City: " << lax.city_ << " --- Country " << lax.country_ << " --- Latitude " << lax.latitude_ << " --- Longitude " << lax.longitude_ << std::endl;
-
-std::cout << std::endl;
-std::cout << "Adjacency List" << std::endl;
-g.populateConnectionsIATA_country(d, "United States");
-std::cout << "Connections From MDW" << std::endl;
-std::vector<std::string> v = g.getConnectionsIATA().find("MDW")->second;
-for (auto a : v) {
-  std::cout << a << ", ";
-}
-std::cout << std::endl;
-std::cout << std::endl;
-std::cout << "Connections From AUS" << std::endl;
-std::vector<std::string> w = g.getConnectionsIATA().find("AUS")->second;
-for (auto a : w) {
-  std::cout << a << ", ";
-}
-std::cout << std::endl;
-std::cout << std::endl;
-std::cout << "BFS Traversal from SAB to SBH" << std::endl;
-g.populateConnectionsIATA(d);
-g.BFS("SAB", "SBH", false);
-std::cout << std::endl;
-std::cout << std::endl;
-std::cout << "BFS Traversal from ORD to LAX" << std::endl;
-g.populateConnectionsIATA(d);
-g.BFS("ORD", "LAX", false);
- return 1;
+// while(true) {
+//   user_control();
+// }
+return 1;
 }
